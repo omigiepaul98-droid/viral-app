b9cc58fe64a6ed213c473294870a877a2ccf11a8
omigiepaul98-droid <YOUR_EMAIL_FOR_THAT_ACCOUNT>
2025-08-31 15:34:17 +0100

    Update for Vercel deployment

--- src/app/lib/supabaseClient.ts
+++ src/app/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://mippgrjetywmscjmpeva.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pcHBncmpldHl3bXNjam1wZXZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxMzQ1NTUsImV4cCI6MjA3MTcxMDU1NX0.MHt138y3TZaxf4vpqKlzFLT8sOgnomcfoI65UC5Cv1E";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
